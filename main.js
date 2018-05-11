38679f6178d1b41381ca99125cf43b3a3b389e03
joedono <zidane320@yahoo.com>
2018-05-11 13:05:28 -0400

    Electron now runs the basic angular project

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+
+let win;
+
+function createWindow () {
+  win = new BrowserWindow({
+    width: 600,
+    height: 600,
+    backgroundColor: '#ffffff',
+    icon: `file://${__dirname}/dist/No-CI/assets/logo.png`
+  });
+
+  win.loadURL(`file://${__dirname}/dist/No-CI/index.html`);
+
+  //// uncomment below to open the DevTools.
+  // win.webContents.openDevTools()
+
+  win.on('closed', function () {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (win === null) {
+    createWindow();
+  }
+});
