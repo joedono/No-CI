50b37a88f8180457025258674c0e34bbce5bb57f
joedono <zidane320@yahoo.com>
2018-05-11 17:49:22 -0400

    Removed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SelectRepoComponent } from './select-repo/select-repo.component';
-import { CommitSelectListComponent } from './commit-select-list/commit-select-list.component';
-import { FileSelectListComponent } from './file-select-list/file-select-list.component';
-import { FtpSettingsFormComponent } from './ftp-settings-form/ftp-settings-form.component';
-import { FtpSettingsComponent } from './ftp-settings/ftp-settings.component';
-import { FileListComponent } from './file-list/file-list.component';
-import { FtpLogComponent } from './ftp-log/ftp-log.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SelectRepoComponent,
-    CommitSelectListComponent,
-    FileSelectListComponent,
-    FtpSettingsFormComponent,
-    FtpSettingsComponent,
-    FileListComponent,
-    FtpLogComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
